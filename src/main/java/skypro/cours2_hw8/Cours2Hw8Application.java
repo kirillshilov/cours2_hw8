5f5deceaddf5b57d99f3daa505053609b655df22
kirillshilov <sarataza@inbox.ru>
2022-05-16 19:17:52 +0300

    Initial commit

---
+++ src/main/java/skypro/cours2_hw8/Cours2Hw8Application.java
@@ -0,0 +1,13 @@
+package skypro.cours2_hw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours2Hw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cours2Hw8Application.class, args);
+    }
+
+}
